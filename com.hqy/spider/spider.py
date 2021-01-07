e4eb85b98b1eb390c2caf4c16cc11933ab34578b
huqunyang <huqunyang@163.com>
2021-01-07 15:05:53 +0800

    init

---
+++ com.hqy/spider/spider.py
@@ -0,0 +1,18 @@
+import urllib
+import bs4
+
+
+# 爬取网页
+def getData(baseUrl):
+    dataList = []
+    return dataList
+
+
+# 得到指定一个URL的网页内容
+def aksURL(url):
+    head = {"User - Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0"}
+
+
+# 保存数据
+def saveData():
+    print("save...")
